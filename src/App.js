8eefdd17eeed0c1798ebacaa4461c3adac0a346b
Facundo <facundoacastello@gmail.com>
2019-08-10 03:41:13 -0300

    Add Errors component & react-router and clean code. closes #2, closes #6

--- src/App.js
+++ src/App.js
@@ -1,34 +1,31 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom"
 import { Provider } from "react-redux";
 
 import BovinesComponent from "./components/BovinesComponent";
-import BrandsComponent from "./components/BrandsComponent";
 
 import store from "./store";
-import logo from "./logo.svg";
 import "./App.css";
-import TransactionsComponent from "./components/TransactionsComponent";
+import Errors from "./components/Errors";
 
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <div className="App">
-          <div className="align-items-center d-flex justify-content-between px-5 w-100 bg-success">
-            <h1 class='mb-0'>Header</h1>
-            <div className="d-flex font-weight-bold w-50 justify-content-around">
-              <div>Action 0</div>
-              <div>Action 1</div>
-              <div>Action 2</div>
-              <div>Action 3</div>
-              <div>Action 4</div>
+
+        <Router>
+          <div className="App">
+            <div className="align-items-center d-flex justify-content-between px-5 w-100 bg-success">
+              <h1 className='mb-0'>Header</h1>
+              <div className="d-flex font-weight-bold w-50 justify-content-around">
+                <Link to="/">Bovines</Link>
+              </div>
+              <div>Users</div>
             </div>
-            <div>Users</div>
+            <Errors />
+            <Route path="/" component={BovinesComponent} />
           </div>
-          <BrandsComponent />
-          <BovinesComponent />
-          <TransactionsComponent />
-        </div>
+        </Router>
       </Provider>
     );
   }
